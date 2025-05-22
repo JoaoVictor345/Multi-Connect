22f7995fb3a81d1aa7f5c9a0d0526fb8ee30f11c
Wtffelp <wtffelp@gmail.com>
2025-05-22 19:54:44 -0300

    ajuste na lista de produtos

--- Home/products.js
+++ Home/products.js
@@ -1,22 +1,11 @@
+// produtos.js
 const produtos = [
-    {
-      nome: "Makita",
-      descricao: "som da makita: zzzzzzzzzzzzzzzzzzzzzzzzh",
-      imagem: "../products/makita.png",
-      link: "#"
-    },
-    {
-      nome: "Pia",
-      descricao: "piupiupiupiupiupiupiupiupiupiu",
-      imagem: "../products/Pia.png",
-      link: "#"
-    },
-    {
-      nome: "Shrek mouses",
-      descricao: "Shrek é foda dms",
-      imagem: "../products/shrek.png",
-      link: "#"
-    },
- 
-  ];
-  
+  { id: 1, nome: "Makita", descricao: "Som da makita  : zzzzzzzzzzh", imagem: "../products/makita.png", preco: 150.99 },
+  { id: 2, nome: "Pia", descricao: "piupiupiupiupiu", imagem: "../products/Pia.png", preco: 299.99 },
+  { id: 3, nome: "Shrek mouses", descricao: "Shrek é foda dms", imagem: "../products/shrek.png", preco: 49.99 },
+  { id: 4, nome: "Carro do bobisponja", descricao: "Carro do bobisponja é foda dms", imagem: "../products/bobisponja.png", preco: 89.99 },
+  { id: 5, nome: "Produto 2", descricao: "Descrição do Produto 2", imagem: "../products/produto2.png", preco: 75.00 },
+  { id: 6, nome: "Produto 1", descricao: "Descrição do Produto 1", imagem: "../products/produto1.png", preco: 35.00 },
+  { id: 7, nome: "Produto 3", descricao: "Descrição do Produto 3", imagem: "../products/produto3.png", preco: 20.00 },
+  { id: 8, nome: "Produto 4", descricao: "Descrição do Produto 4", imagem: "../products/produto4.png", preco: 99.99 }
+];
