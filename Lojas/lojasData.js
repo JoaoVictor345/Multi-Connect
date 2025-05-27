935882b208e5c788988669bf7e643bd68ba18877
wtffelp <martinsjvictor4@gmail.com>
2025-05-27 20:56:22 -0300

    Lojas.html melhorias

--- Lojas/lojasData.js
+++ Lojas/lojasData.js
@@ -4,70 +4,70 @@ const lojas = [
     nome: "TechWave",
     descricao: "Loja especializada em eletrônicos e gadgets de última geração.",
     avaliacao: "⭐⭐⭐⭐",
-    imagem: "../products/techwave.png",
+    imagem: "../imgs/techwave.avif",
     link: "#" 
   },
   { 
     nome: "Casa Verde",
     descricao: "Produtos sustentáveis para o lar e jardim, com foco em ecologia.",
     avaliacao: "⭐⭐⭐⭐⭐", 
-    imagem: "../products/casaverde.png",
+    imagem: "../imgs/casaverde.jpg",
     link: "#" 
   },
   { 
     nome: "Bella Moda",
     descricao: "Moda feminina contemporânea com coleções exclusivas.",
     avaliacao: "⭐⭐⭐", 
-    imagem: "../products/bellamoda.png",
+    imagem: "../imgs/bellamoda.avif",
     link: "#" 
   },
   { 
     nome: "Livros & Cia", 
     descricao: "A livraria perfeita para quem ama clássicos e lançamentos.", 
     avaliacao: "⭐⭐⭐⭐⭐", 
-    imagem: "../products/livroscia.png",
+    imagem: "../imgs/betterbooks.avif",
     link: "#" 
   },
   { 
     nome: "PetAlegria",
     descricao: "Tudo para seu pet: ração, brinquedos, acessórios e carinho.",
     avaliacao: "⭐⭐⭐⭐",
-    imagem: "../products/petalegria.png",
+    imagem: "../imgs/homepets.avif",
     link: "#" 
   },
   { 
     nome: "Urban Bikes",
     descricao: "Bicicletas urbanas e elétricas com design moderno e desempenho top.",
     avaliacao: "⭐⭐⭐⭐",
-    imagem: "../products/shrek.png",
+    imagem: "../imgs/bicycle.avif",
     link: "#" 
   },
   {
    nome: "Doces Encantos", 
    descricao: "Confeitaria artesanal com doces que encantam pelo sabor e visual.",
    avaliacao: "⭐⭐⭐⭐⭐",
-   imagem: "../products/shrek.png",
+   imagem: "../imgs/doces.avif",
    link: "#" 
   },
   { 
     nome: "ClickTech",
      descricao: "Equipamentos de informática e assistência técnica especializada.",
      avaliacao: "⭐⭐⭐",
-     imagem: "../products/clicktech.png",
+     imagem: "../imgs/clicktech.avif",
      link: "#" 
   },
   { 
     nome: "Aroma Zen",
      descricao: "Velas, óleos e itens para aromaterapia e bem-estar.",
      avaliacao: "⭐⭐⭐⭐⭐",
-     imagem: "../products/aromazen.png",
+     imagem: "../imgs/aroma_zen.png",
      link: "#"
   },
   { 
     nome: "Mundo Geek",
     descricao: "Produtos e colecionáveis para fãs de séries, HQs, games e animes.",
     avaliacao: "⭐⭐⭐⭐",
-    imagem: "../products/mundogeek.png",
+    imagem: "../imgs/geek_gaming.jpg",
     link: "#"
   }
 ];
