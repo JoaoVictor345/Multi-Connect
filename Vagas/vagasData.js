bd028bbde64f3d465b9efa9bc2d9e190c305912a
Wtffelp <wtffelp@gmail.com>
2025-05-29 18:27:20 -0300

    paginas de vagas

---
+++ Vagas/vagasData.js
@@ -0,0 +1,32 @@
+const vagas = [
+  {
+    titulo: "Assistente Administrativo",
+    descricao: "Organização de documentos, atendimento ao público e suporte ao setor administrativo.",
+    imagem: "../imgs/vaga1.png",
+    link: "#"
+  },
+  {
+    titulo: "Desenvolvedor Front-End",
+    descricao: "Desenvolvimento de interfaces responsivas em HTML, CSS e JavaScript.",
+    imagem: "../imgs/vaga2.png",
+    link: "#"
+  },
+  {
+    titulo: "Analista de Suporte Técnico",
+    descricao: "Atendimento e suporte aos usuários, manutenção de hardware e software.",
+    imagem: "../imgs/vaga3.png",
+    link: "#"
+  },
+  {
+    titulo: "Marketing Digital",
+    descricao: "Criação de campanhas, gestão de redes sociais e análise de métricas.",
+    imagem: "../imgs/vaga4.png",
+    link: "#"
+  },
+  {
+    titulo: "Estágio em RH",
+    descricao: "Apoio nos processos seletivos, triagem de currículos e entrevistas.",
+    imagem: "../imgs/vaga5.png",
+    link: "#"
+  }
+];
